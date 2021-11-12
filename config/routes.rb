1189bbdd2c30cbe125d4effca37bb1b266a8194f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-12 10:44:21 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, except: [:new]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
-  get '/signup' => 'users#new'
-  root to: 'users#welcome'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
+
 end
