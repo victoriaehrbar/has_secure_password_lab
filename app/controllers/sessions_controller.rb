d3708878dd7fef9a4e8b15ff54a7799ca6415b7f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-11-12 10:41:17 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,24 @@
 class SessionsController < ApplicationController
   def new
-      
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id 
-      redirect_to user_path(@user)
-    else
-      redirect_to signup_path
-    end
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
-      session.delete(:user_id)    
-      session[:user_id] = nil         
-      redirect_to '/' 
+    session.delete :user_id
+
+    redirect_to '/'
   end
 end
